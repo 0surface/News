037f165a58705570268a0d84a50cbf922dde5311
0surface <tms.0void@gmail.com>
2019-04-09 11:11:18 +0100

    Added Util project to handle web scraping and d3 node construction

---
+++ News.Util/D3Tree/D3TreeNodeMaker.cs
@@ -0,0 +1,66 @@
+﻿using News.Types.D3Tree;
+using News.Types.Website;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace News.Util.D3Tree
+{
+    public class D3TreeNodeMaker
+    {
+        public static Func<string, string, List<D3TreeNode>, D3TreeNode> ConstructNode;
+        public static Func<List<string>, List<D3TreeNode>> GetNodesFromStringList;
+
+        public static int InjectDependencies()
+        {
+            ConstructNode = _ConstructNode();
+            GetNodesFromStringList = _GetNodesFromStringList();
+
+            return 0;
+        }
+
+        private static Func<string, string, List<D3TreeNode>, D3TreeNode> _ConstructNode()
+            =>  (name, parent, nodeList) =>
+            {
+                try
+                {
+                    D3TreeNode node = new D3TreeNode(name, parent);
+
+                    if(nodeList != null && nodeList.Count > 0)
+                    {
+                        nodeList.ForEach(x => x.Parent = name);
+                    }
+                    node.Children = nodeList;
+
+                    return node;
+                }
+                catch (Exception)
+                {
+                    return D3TreeNode.Empty();
+                }
+            };
+
+        private static Func<List<string>, List<D3TreeNode>> _GetNodesFromStringList()
+          => (stringList) =>
+          {
+              try
+              {
+                  List<D3TreeNode> nodes = new List<D3TreeNode>();
+
+                  if (stringList != null && stringList.Count > 0)
+                  {
+                      foreach (var item in stringList)
+                      {
+                          nodes.Add(new D3TreeNode(item, ""));
+                      }
+                  }
+
+                  return nodes;
+              }
+              catch (Exception)
+              {
+                  return new List<D3TreeNode>();
+              }
+          };               
+    }
+}
---
+++ News.Util/WebScraper/WebScraper.cs
@@ -0,0 +1,119 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace News.Util.WebScraper
+{
+    public class WebScraper
+    {
+        public static Func<string, Task<HtmlDocument>> GetHtmlDocument;
+        public static Func<HtmlDocument, string, Task<List<string>>> GetRawHeadlines;
+        public static Func<List<string>, Task<List<string>>> CleanHeadlines;
+        public static Func<List<string>, int, List<string>> GetLimitedHeadlines;
+
+        public static int InjectDependencies()
+        {
+            GetHtmlDocument = _GetHtmlDocument();
+            GetRawHeadlines = _GetRawHeadlines();
+            CleanHeadlines = _CleanHeadlines();
+            GetLimitedHeadlines = _GetLimitedHeadlines();
+
+            return 0;
+        }
+
+        private static Func<string, Task<HtmlDocument>> _GetHtmlDocument()
+            => async (url) =>
+        {
+            try
+            {
+                HtmlWeb web = new HtmlWeb();
+                return await web.LoadFromWebAsync(url);
+
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        };
+
+        private static Func<HtmlDocument, string, Task<List<string>>> _GetRawHeadlines()
+            => async (htmlDoc, headlineSelector) =>
+        {
+            try
+            {
+                return htmlDoc.DocumentNode
+                                .SelectNodes(headlineSelector)
+                                .Select(node => node.InnerText.Replace("&#x27;", "'"))
+                                .ToList();
+            }
+            catch (Exception)
+            {
+                return new List<string>();
+            }
+        };
+
+        private static Func<List<string>, Task<List<string>>> _CleanHeadlines()
+            => async (rawHeadlines) =>
+        {
+            try
+            {
+                if (rawHeadlines == null || rawHeadlines.Count < 1)
+                    return rawHeadlines;
+
+                foreach (var item in rawHeadlines)
+                {
+                    item.Replace("&#x27;", "'");
+                }
+
+                return rawHeadlines;
+            }
+            catch (Exception)
+            {
+                return rawHeadlines;
+            }
+        };
+
+        private static Func<List<string>, int, List<string>> _GetLimitedHeadlines()
+           => (rawHeadlines, limit) =>
+          {
+              try
+              {
+                  if (rawHeadlines == null || rawHeadlines.Count < 1)
+                      return rawHeadlines;
+
+                  return rawHeadlines.Take(limit).ToList();
+              }
+              catch (Exception)
+              {
+                  return rawHeadlines;
+              }
+          };
+
+
+        //private async Task<List<D3TreeNode>> GetWebData(string url, string headlineSelector)
+        //{
+        //    HtmlWeb web = new HtmlWeb();
+        //    List<D3TreeNode> nodes = new List<D3TreeNode>();
+        //    HtmlDocument doc = await web.LoadFromWebAsync(url);
+
+        //    var headerNames = doc.DocumentNode
+        //        .SelectNodes(headlineSelector)
+        //        .Select(node => node.InnerText.Replace("&#x27;", "'"))
+        //        .ToList();
+
+        //    int limit = 0;
+        //    foreach (var header in headerNames)
+        //    {
+        //        if (limit > 10) return nodes;
+
+        //        nodes.Add(new D3TreeNode(HttpUtility.HtmlDecode(header), ""));
+        //        limit++;
+        //    }
+
+        //    return nodes;
+        //}
+
+    }
+}
