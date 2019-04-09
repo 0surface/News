fb33cb80f82f18e7fa61f1cab0ad30d6ce34b69f
0surface <tms.0void@gmail.com>
2019-04-09 11:10:33 +0100

    Added types project to house common models

---
+++ News.Types/D3Tree/D3TreeNode.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace News.Types.D3Tree
+{
+    public class D3TreeNode
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("parent")]
+        public string Parent { get; set; }
+        [JsonProperty("children")]
+        public List<D3TreeNode> Children { get; set; }
+
+        public D3TreeNode(string name, string parent, List<D3TreeNode> children)
+        {
+            Name = name;
+            Parent = parent;
+            Children = children;
+        }
+
+        public D3TreeNode(string name, string parent)
+        {
+            Name = name;
+            Parent = parent;
+            Children = null;
+        }
+
+        public static D3TreeNode Empty() => new D3TreeNode("", "");
+        
+    }
+}
---
+++ News.Types/Website/WebsiteDto.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace News.Types.Website
+{
+    public class WebsiteDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CanonicalUrl { get; set; }
+        public string LogoImageUrl { get; set; }
+        public string HeadLineSelector { get; set; }
+        public List<string> SubUrls { get; set; }
+    }
+}
